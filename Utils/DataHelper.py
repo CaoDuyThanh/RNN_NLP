ddae35151084ab64d585e471d1d2aaf880b1d0a6
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-01 00:15:45 +0700

    Modified:
    - Add Sentence start and end token

--- Utils/DataHelper.py
+++ Utils/DataHelper.py
@@ -2,6 +2,9 @@ import csv
 import itertools
 import nltk
 
+sentenceStartToken = "SENTENCE_START"
+sentenceEndToken = "SENTENCE_END"
+
 def LoadData(fileName):
     print ('Reading CSV file...')
 
@@ -11,7 +14,7 @@ def LoadData(fileName):
         # Split full comments into sentences
         sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
         # Append SETENCE_START and SETENCE_END
-        sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+        sentences = ["%s %s %s" % (sentenceStartToken, x, sentenceEndToken) for x in sentences]
     print ('Reading CSV file...Done! ')
     print "Parsed %d sentences." % (len(sentences))
 
