212ef98e7c18e767080b9a4c164ce5976fcce69d
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-01 00:16:47 +0700

    Modified:
    - Create train / valid model, cost function

--- Networks/RNN.py
+++ Networks/RNN.py
@@ -1,41 +1,48 @@
+import numpy
+import theano
+import theano.tensor as T
 import pickle
 import cPickle
 from Layers.RNNHiddenLayer import *
 from Layers.SoftmaxLayer import *
+from Utils.CostFHelper import *
 
 class RNN:
     def __init__(self,
                  rng,
-                 input,
                  numIn,
                  numHidden,
                  truncate,
-                 useSoftmax=False,
-                 activation = T.tanh):
+                 useSoftmax = False,
+                 activation = T.tanh,
+                 learningRate = 0.005):
         # Set parameters
         self.Rng = rng
-        self.Input = input
         self.NumIn = numIn
         self.NumHidden = numHidden
         self.Truncate = truncate
         self.UseSoftmax = useSoftmax
         self.Activation = activation
-        self.Layers = []
+        self.LearningRate = learningRate
 
+        self.createRNN()
+        self.buildRNN()
+
+    def createRNN(self):
         # Save shared parameters
         self.Whh = None
         self.Wx = None
         self.Wy = None
 
         # Create RNN model
+        self.Layers = []
         for layerId in range(self.Truncate):
-            hiddenLayer = None
             if layerId == 0:
                 hiddenLayer = RNNHiddenLayer(
-                    rng = self.Rng,
-                    numIn = self.NumIn,
-                    numHidden = self.NumHidden,
-                    activation = self.Activation
+                    rng=self.Rng,
+                    numIn=self.NumIn,
+                    numHidden=self.NumHidden,
+                    activation=self.Activation
                 )
                 self.Whh = hiddenLayer.Whh
                 self.Wx = hiddenLayer.Wx
@@ -46,15 +53,58 @@ class RNN:
                     numIn=self.NumIn,
                     numHidden=self.NumHidden,
                     activation=self.Activation,
-                    whh = self.Whh,
-                    wx = self.Wx,
-                    wy = self.Wy
+                    whh=self.Whh,
+                    wx=self.Wx,
+                    wy=self.Wy
                 )
-            self.Layers += hiddenLayer
+            self.Layers.append(hiddenLayer)
 
-        # Update parameters
-        params = [layer.Params for layer in self.Layers]
-        grads = T.grad(cost)
+    def buildRNN(self):
+        # Create train model
+        X = T.matrix('X', dtype = theano.config.floatX)
+        Y = T.matrix('Y', dtype = theano.config.floatX)
+
+        # Feed-forward
+        S = numpy.zeros((1, self.NumHidden), dtype = theano.config.floatX)
+        for idx, layer in enumerate(self.Layers):
+            [S, Yp] = layer.FeedForward(S, X[idx])
+
+        # Calculate cost function
+        softmaxLayer = SoftmaxLayer(Yp)
+        Yp = softmaxLayer.Output()
+        cost = CategoryEntropy(Yp, Y)
+
+        # Calculate error function
+
+        # Get params and calculate gradients
+        params = self.Layers[-1].Params
+        grads = T.grad(cost, params)
+        updates = [(param, param - self.LearningRate * grad)
+                   for (param, grad) in zip(params, grads)]
+
+        self.TrainModel = theano.function(
+            inputs = [X, Y],
+            outputs = cost,
+            updates = updates,
+        )
+
+        # Create test model
+        # self.TestModel = theano.function(
+        #     inputs=[X, Y],
+        #     outputs=rnnModel.Cost(Y),
+        #     updates=rnnModel.Updates()
+        # )
+
+    def Output(self):
+        output = self.Layers[-1].Output
+        if self.UseSoftmax is False:
+            return output
+        else:
+            softmaxLayer = SoftmaxLayer(
+                input = output
+            )
+            return softmaxLayer.Output()
+
 
 
     def LoadModel(self, fileName):
@@ -70,18 +120,4 @@ class RNN:
         pickle.dump(self.Wx.get_value(borrow = True), file, -1)
         pickle.dump(self.Wy.get_value(borrow = True), file, -1)
         file.close()
-
-    def Output(self):
-        output = self.Layers[-1].Output
-        if self.UseSoftmax is False:
-            return output
-        else:
-            softmaxLayer = SoftmaxLayer(
-                input = output
-            )
-            return softmaxLayer.Output()
-
-    def Cost(self):
-        return 0
-
 
