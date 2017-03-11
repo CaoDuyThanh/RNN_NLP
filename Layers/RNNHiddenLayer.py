a8ec285656d8732e64dd495266c4d8bc058b6365
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-11 22:51:39 +0700

    Modified:
    - Add activation for output

--- Layers/RNNHiddenLayer.py
+++ Layers/RNNHiddenLayer.py
@@ -12,7 +12,8 @@ class RNNHiddenLayer:
                  whh = None,
                  wx = None,
                  wy = None,
-                 activation = T.tanh):
+                 sActivation = T.tanh,
+                 yActivation = None):
         # Set parameters
         self.Rng = rng
         self.NumIn = numIn
@@ -20,17 +21,18 @@ class RNNHiddenLayer:
         self.Whh = whh
         self.Wx = wx
         self.Wy = wy
-        self.Activation = activation
+        self.SActivation = sActivation
+        self.YActivation = yActivation
 
-        self.createHiddenLayer()
-        self.buildHiddenLayer()
+        self.createModel()
 
-    def createHiddenLayer(self):
+    def createModel(self):
         if self.Whh is None:
+            wBound = numpy.sqrt(6.0 / (self.NumHidden + self.NumHidden))
             self.Whh = theano.shared(
                 numpy.asarray(self.Rng.uniform(
-                        low = -1.0,
-                        high = 1.0,
+                        low  = -wBound,
+                        high =  wBound,
                         size = (self.NumHidden, self.NumHidden)
                     ),
                     dtype = theano.config.floatX
@@ -39,34 +41,38 @@ class RNNHiddenLayer:
             )
 
         if self.Wx is None:
+            wBound = numpy.sqrt(6.0 / (self.NumIn + self.NumHidden))
             self.Wx = theano.shared(
                 numpy.asarray(self.Rng.uniform(
-                    low = -1.0,
-                    high = 1.0,
-                    size = (self.NumIn, self.NumHidden)
-                ),
+                        low  = -wBound,
+                        high =  wBound,
+                        size = (self.NumIn, self.NumHidden)
+                    ),
                     dtype = theano.config.floatX
                 ),
                 borrow = True
             )
 
         if self.Wy is None:
+            wBound = numpy.sqrt(6.0 / (self.NumHidden + self.NumIn))
             self.Wy = theano.shared(
                 numpy.asarray(self.Rng.uniform(
-                    low = -1.0,
-                    high = 1.0,
-                    size = (self.NumHidden, self.NumIn)
-                ),
+                        low  = -wBound,
+                        high =  wBound,
+                        size = (self.NumHidden, self.NumIn)
+                    ),
                     dtype = theano.config.floatX
                 ),
                 borrow = True
             )
 
-    def buildHiddenLayer(self):
         self.Params = [self.Whh, self.Wx, self.Wy]
 
     def FeedForward(self, Skm1, Xk):
-        S = self.Activation(T.dot(Xk, self.Wx) + T.dot(Skm1, self.Whh))
-        Y = T.dot(S, self.Wy)
+        S = self.SActivation(self.Wx[Xk] + T.dot(Skm1, self.Whh))
+        if self.YActivation is None:
+            Y = T.dot(S, self.Wy)
+        else:
+            Y = self.YActivation(T.dot(S, self.Wy))
         return [S, Y]
 
