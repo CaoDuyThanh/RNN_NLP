9bd8dfdda54e174e23d7c9e6d22f999ed0d916ff
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-11 22:53:51 +0700

    Modified:
    - Refactor code

--- Utils/CostFHelper.py
+++ Utils/CostFHelper.py
@@ -5,13 +5,12 @@ import theano.tensor as T
 def L1(W):
     return abs(W).sum()
 
-""" L2 - Regularization """# return -T.mean(T.neq(T.argmax(output), T.argmax(y)))
+""" L2 - Regularization """
 def L2(W):
     return abs(W ** 2).sum()
 
 """ Cross entropy """
 def CrossEntropy(output, y):
-    # return T.mean(T.sum(T.nnet.binary_crossentropy(output, y), 1))
     return -T.mean(T.log(output)[T.arange(y.shape[0]), y])
 
 """ Category entropy """
@@ -20,5 +19,4 @@ def CategoryEntropy(output, y):
 
 """ Error """
 def Error(output, y):
-    # return -T.mean(T.neq(T.argmax(output), T.argmax(y)))
     return T.mean(T.neq(T.argmax(output, 1), y))
