ac5c513d74312e54b0cb714e134b90819d128246
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-11 22:52:50 +0700

    Modified:
    - Change cost function
    - Integrate softmax activation to hidden layer

--- Networks/RNN.py
+++ Networks/RNN.py
@@ -1,10 +1,6 @@
-import numpy
-import theano
-import theano.tensor as T
 import pickle
 import cPickle
 from Layers.RNNHiddenLayer import *
-from Layers.SoftmaxLayer import *
 from Utils.CostFHelper import *
 
 class RNN:
@@ -13,7 +9,6 @@ class RNN:
                  numIn,
                  numHidden,
                  truncate,
-                 useSoftmax = False,
                  activation = T.tanh,
                  learningRate = 0.005):
         # Set parameters
@@ -21,12 +16,10 @@ class RNN:
         self.NumIn = numIn
         self.NumHidden = numHidden
         self.Truncate = truncate
-        self.UseSoftmax = useSoftmax
         self.Activation = activation
         self.LearningRate = learningRate
 
         self.createRNN()
-        self.buildRNN()
 
     def createRNN(self):
         # Save shared parameters
@@ -39,58 +32,65 @@ class RNN:
         for layerId in range(self.Truncate):
             if layerId == 0:
                 hiddenLayer = RNNHiddenLayer(
-                    rng=self.Rng,
-                    numIn=self.NumIn,
-                    numHidden=self.NumHidden,
-                    activation=self.Activation
+                    rng        = self.Rng,
+                    numIn      = self.NumIn,
+                    numHidden  = self.NumHidden,
+                    sActivation = self.Activation
                 )
                 self.Whh = hiddenLayer.Whh
-                self.Wx = hiddenLayer.Wx
-                self.Wy = hiddenLayer.Wy
+                self.Wx  = hiddenLayer.Wx
+                self.Wy  = hiddenLayer.Wy
             else:
-                hiddenLayer = RNNHiddenLayer(
-                    rng=self.Rng,
-                    numIn=self.NumIn,
-                    numHidden=self.NumHidden,
-                    activation=self.Activation,
-                    whh=self.Whh,
-                    wx=self.Wx,
-                    wy=self.Wy
-                )
+                if layerId == self.Truncate - 1:
+                    hiddenLayer = RNNHiddenLayer(
+                        rng         = self.Rng,
+                        numIn       = self.NumIn,
+                        numHidden   = self.NumHidden,
+                        sActivation = self.Activation,
+                        yActivation = T.nnet.softmax,
+                        whh  = self.Whh,
+                        wx   = self.Wx,
+                        wy   = self.Wy
+                    )
+                else:
+                    hiddenLayer = RNNHiddenLayer(
+                        rng         = self.Rng,
+                        numIn       = self.NumIn,
+                        numHidden   = self.NumHidden,
+                        sActivation = self.Activation,
+                        whh  = self.Whh,
+                        wx   = self.Wx,
+                        wy   = self.Wy
+                    )
             self.Layers.append(hiddenLayer)
 
-    def buildRNN(self):
         # Create train model
-        X = T.matrix('X', dtype = theano.config.floatX)
-        Y = T.matrix('Y', dtype = theano.config.floatX)
+        X = T.ivector('X')
+        Y = T.ivector('Y')
 
         # Feed-forward
-        S = numpy.zeros((1, self.NumHidden), dtype = theano.config.floatX)
+        S = numpy.ones((1, self.NumHidden), dtype = theano.config.floatX)
         for idx, layer in enumerate(self.Layers):
             [S, Yp] = layer.FeedForward(S, X[idx])
 
-        # Calculate cost function
-        softmaxLayer = SoftmaxLayer(Yp)
-        Yp = softmaxLayer.Output()
-        cost = CategoryEntropy(Yp, Y)
-
-        # Calculate error function
+        # Calculate cost | error function
+        cost = CrossEntropy(Yp, Y)
 
         # Get params and calculate gradients
         params = self.Layers[-1].Params
-        grads = T.grad(cost, params)
+        grads  = T.grad(cost, params)
         updates = [(param, param - self.LearningRate * grad)
                    for (param, grad) in zip(params, grads)]
-        self.TrainModel = theano.function(
-            inputs = [X, Y],
+        self.TrainFunc = theano.function(
+            inputs  = [X, Y],
             outputs = cost,
             updates = updates,
         )
 
         # Create test model
-        self.TestModel = theano.function(
-            inputs = [X, Y],
-            outputs = cost
+        self.TestFunc = theano.function(
+            inputs    = [X, Y],
+            outputs   = cost
         )
 
     def LoadModel(self, fileName):
@@ -106,4 +106,3 @@ class RNN:
         pickle.dump(self.Wx.get_value(borrow = True), file, -1)
         pickle.dump(self.Wy.get_value(borrow = True), file, -1)
         file.close()
-
